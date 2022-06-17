d2aed7d157b54380f68cc986acbde16c69a08f88
Paul Ogolla <paulotieno2@gmail.com>
2022-06-17 08:08:10 +0300

    build assignment

--- I4G007066QBW/blog/models.py
+++ I4G007066QBW/blog/models.py
@@ -1,3 +1,12 @@
+from turtle import mode, title
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=1500)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    created_date = models.DateTimeField('date published')
+    published_date = models.DateTimeField('date published')
